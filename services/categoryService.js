bcbe080d5ce3d07262c40d71091c2bc8cc8f01bb
MasujimaNobuyasu <masujimanobuyasu@MasujimaNobuyasu-no-MacBook-Air.local>
2015-12-24 15:33:00 +0900

    implement post category

--- services/categoryService.js
+++ services/categoryService.js
@@ -1,27 +1,40 @@
 'use strict';
-var schema = require('../schemas/categorySchema');
-var Category = schema.Category;
-var service = {};
-module.exports = service;
+const schema = require('../schemas/categorySchema');
+const Category = schema.Category;
 
-/**
- * 全カテゴリを取得する
- * @returns Promise
- */
-service.fetchList = function () {
-  return new Promise(function (resolve, reject) {
-    Category.find({}, function(err, items){
-      resolve(items);
-      if (err) {
-        reject(err);
-      } else {
+class CategoryService {
+ /**
+  * Fetch Categories
+  * @returns Promise
+  */
+  fetchList() {
+    return new Promise(function (resolve, reject) {
+      Category.find({}, function(err, items){
         resolve(items);
-      }
+        if (err) {
+          reject(err);
+        } else {
+          resolve(items);
+        }
+      });
+    });    
+  }
+
+ /**
+  * Add NewCategory
+  * @returns Promise
+  */  
+  add(addData) {
+    return new Promise(function (resolve, reject) {
+      Category.save(addData, function (err, result) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      });
     });
-  });
-};
+  }
+}
 
-
-
-
-
+module.exports = CategoryService;
